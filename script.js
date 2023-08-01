7559d048f3d6d62496150365522f9a5aa18b7d98
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-01 18:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let div=document.CreateElement('div');
+div.id='browser-info';
+document.body.appendChild(div);
+div.innerText="You are using "+Navigator.appName+" version "+Navigator.appVersion;
